1684ba9ba8a1a97c707142188cb0d038d6b70fec
Abhishek Sharma <abhitiku2003@gmail.com>
2025-02-22 17:30:27 +0530

    switched to repository framework DI for API fetch

--- Mars App/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ Mars App/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -16,7 +16,3 @@ interface MarsApiService {
     @GET("photos")
     suspend fun getPhotos(): List<MarsPhoto>
 }
-
-object MarsApi {
-    val retrofitService : MarsApiService by lazy {retrofit.create(MarsApiService::class.java)}
-}
